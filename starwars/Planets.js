c7f691f63c5fd396838b6af9352e0a3627e3133e
ambsprad35 <aspradley2@ivytech.edu>
2025-11-15 22:40:30 -0500

    Update Planets.js
    updated to get info from star wars api and store data in a list

--- starwars/Planets.js
+++ starwars/Planets.js
@@ -1,11 +1,50 @@
-import React from "react";
-import { View, Text } from "react-native";
+import React, {useEffect, useState} from "react";
+import { View, Text, FlatList, ActivityIndicator, StyleSheet } from "react-native";
 import styles from "./styles";
 
+
 export default function Planets() {
+  /* Create Arrays */
+  const [planets, setPlanets] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  /* Fetch Star Wars Api */
+  const fetchPlanets = async () => {
+    try {
+      const response = await fetch("https://swapi.dev/api/planets/");
+      const json = await response.json();
+      setPlanets(json.results);
+    } catch (err) {
+      setError("Failed to load planets");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchPlanets();
+  }, []);
+
+  /* Display either loading indicator or error message */
+  if(loading) return <ActivityIndicator size="large" style={{ marginTop: 50}} />;
+  if(error) return <Text style={styles.error}>{error}</Text>;
+  
   return (
+    /* Display info from Star Wars API as a list */
     <View style={styles.container}>
       <Text>Planets Content</Text>
+      <FlatList
+        data={planets}
+        keyExtractor={(item) => item.url}
+        renderItem={({item}) => (
+          <View style={styles.item}>
+            <Text style={styles.name}>{item.name}</Text>
+            <Text>Climate: {item.climate}</Text>
+            <Text>Population: {item.population}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
