6a14b816bf40d88c95e23a463fb3c23cd17d9434
ambsprad35 <aspradley2@ivytech.edu>
2025-11-15 22:41:48 -0500

    Update styles.js
    updated for list displays

--- starwars/styles.js
+++ starwars/styles.js
@@ -3,7 +3,22 @@ import { StyleSheet } from "react-native";
 export default StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    padding: 15,
   },
+  item: {
+    padding: 15,
+    marginBottom: 10,
+    backgroundColor: "#eee",
+    borderRadius: 10,
+  },
+  name: {
+    fontWeight: "bold",
+    fontSize: 18
+  },
+  error: {
+    color: "red",
+    padding: 20,
+    textAlign: "center"
+  }
+  
 });
