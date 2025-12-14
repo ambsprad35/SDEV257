ef79fbb71da53b48cca68274b8c9a5d5b9893e7c
ambsprad35 <aspradley2@ivytech.edu>
2025-12-14 17:10:52 -0500

    Update App.js

--- starwars/App.js
+++ starwars/App.js
@@ -1,34 +1,33 @@
 import 'react-native-gesture-handler';
-import React from "react";
-import { NavigationContainer } from "@react-navigation/native";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { Platform, View, Text, ActivityIndicator } from "react-native";
-import { GestureHandlerRootView } from "react-native-gesture-handler";
-import NetInfo from "@react-native-community/netinfo";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Platform, View, Text, ActivityIndicator } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import NetInfo from '@react-native-community/netinfo';
 
-import styles from "./styles";
-import Planets from "./Planets";
-import Films from "./Films";
-import Spaceships from "./Spaceships";
+import styles from './styles';
+import PlanetStack from './navigation/PlanetNav';
+import Planets from './screens/Planets';
+import Films from './screens/Films';
+import Spaceships from './screens/Spaceships';
 
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
 
 export default function App() {
-
   const [isConnected, setIsconnected] = React.useState(null);
 
   // Subscribe to network status changes
   React.useEffect(() => {
     const handleNetInfo = (state) => {
-
       // Web override
-      if (Platform.OS === "web") {
+      if (Platform.OS === 'web') {
         setIsconnected(navigator.onLine);
         return;
       }
-      const reachable = 
+      const reachable =
         state.isInternetReachable === null
           ? state.isConnected
           : state.isConnected && state.isInternetReachable;
@@ -42,25 +41,25 @@ export default function App() {
     NetInfo.fetch().then(handleNetInfo);
 
     // Add listeners for network reconnection for web
-    if (Platform.OS === "web") {
+    if (Platform.OS === 'web') {
       const goOnline = () => setIsconnected(true);
       const goOffline = () => setIsconnected(false);
 
-      window.addEventListener("online", goOnline);
-      window.addEventListener("offline", goOffline);
+      window.addEventListener('online', goOnline);
+      window.addEventListener('offline', goOffline);
 
       return () => {
-        window.removeEventListener("online", goOnline);
-        window.removeEventListener("offline", goOffline);
+        window.removeEventListener('online', goOnline);
+        window.removeEventListener('offline', goOffline);
         unsubscribe();
-    };
-  }
+      };
+    }
 
     return () => unsubscribe();
   }, []);
 
   // Show loading indicator for a slow loading network
-  if(isConnected === null){
+  if (isConnected === null) {
     return (
       <View style={styles.center}>
         <ActivityIndicator size="large" />
@@ -77,46 +76,56 @@ export default function App() {
           Please check your network connection.
         </Text>
       </View>
-    )
+    );
   }
 
   // The network is online and the app will load
   return (
-    <GestureHandlerRootView style={{ flex: 1}}>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <NavigationContainer>
-        {Platform.OS === "ios" ? (
-          <Tab.Navigator screenOptions={{ swipeEnabled: true}}>
-            <Tab.Screen name="Planets">{() => <Planets />}</Tab.Screen>
-            <Tab.Screen name="Films">{() => <Films />}</Tab.Screen>
-            <Tab.Screen name="Spaceships">{() => <Spaceships />}</Tab.Screen>
-          </Tab.Navigator>
-        ) : Platform.OS === "android" ? (
-          <Drawer.Navigator
-            screenOptions={{
-              swipeEnabled: true,
-              gestureEnabled: true,
-            }}>
-            <Drawer.Screen name="Planets">{() => <Planets />}</Drawer.Screen>
-            <Drawer.Screen name="Films">{() => <Films />}</Drawer.Screen>
-            <Drawer.Screen name="Spaceships">
-              {() => <Spaceships />}
-            </Drawer.Screen>
+        {Platform.OS === 'android' ? (
+          <Drawer.Navigator>
+            <Drawer.Screen
+              name="Planets"
+              children={(props) => (
+                <PlanetStack {...props} isConnected={isConnected} />
+              )}
+            />
+            <Drawer.Screen
+              name="Films"
+              children={(props) => <Films {...props} />}
+            />
+            <Drawer.Screen
+              name="Spaceships"
+              children={(props) => <Spaceships {...props} />}
+            />
           </Drawer.Navigator>
         ) : (
-        <Tab.Navigator>
-          <Tab.Screen 
-            name="Planets"
-            children={() => <Planets isConnected={isConnected} />}
-          />
-          <Tab.Screen 
-            name="Films"
-            children={() => <Films />}
-          />
-          <Tab.Screen 
-            name="Spaceships"
-            children={() => <Spaceships />}
-          />
-        </Tab.Navigator>
+          <Tab.Navigator>
+            <Tab.Screen
+              name="Planets"
+              children={(props) => (
+                <PlanetStack {...props} isConnected={isConnected} />
+              )}
+              options={{
+                gestureEnabled: false, 
+              }}
+            />
+            <Tab.Screen
+              name="Films"
+              children={(props) => <Films {...props} />}
+              options={{
+                gestureEnabled: false, 
+              }}
+            />
+            <Tab.Screen
+              name="Spaceships"
+              children={(props) => <Spaceships {...props} />}
+              options={{
+                gestureEnabled: false, 
+              }}
+            />
+          </Tab.Navigator>
         )}
       </NavigationContainer>
     </GestureHandlerRootView>
