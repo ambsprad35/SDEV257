3f05373344176a8761e64449347fb877d82b9957
ambsprad35 <aspradley2@ivytech.edu>
2025-12-14 17:03:01 -0500

    Update PlanetNav.js

--- starwars/navigation/PlanetNav.js
+++ starwars/navigation/PlanetNav.js
@@ -1,1 +1,23 @@
+import React from "react";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import PlanetsScreen from "../screens/Planets";
+import PlanetDetailScreen from "../screens/PlanetDetails";
 
+const Stack = createNativeStackNavigator();
+
+export default function PlanetStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen 
+        name="Planets" 
+        component={PlanetsScreen} 
+        options={{ headerShown: false }} 
+      />
+      <Stack.Screen 
+        name="PlanetDetails" 
+        component={PlanetDetailScreen} 
+        options={{ title: "Planet Details" }} 
+      />
+    </Stack.Navigator>
+  );
+}
